0fb9be5a11a0f2b772bc4efa61656522304e0018
DESKTOP-M30FNO0\GA2A <h1202419010@hiratagakuen.onmicrosoft.com>
2025-05-22 11:30:43 +0900

    Playerの移動兼視点移動完成

--- teamproject/Assets/Script/FPSController.cs
+++ teamproject/Assets/Script/FPSController.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class FPSController : MonoBehaviour
 {
+
+    public float mouseSensitivity = 100f;
+    public float xRotation = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.parent.Rotate(Vector3.up * mouseX);
     }
 }
