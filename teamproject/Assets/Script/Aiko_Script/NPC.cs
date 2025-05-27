ddb4e650b71cee764b9fb8fbd4146e21b344d738
DESKTOP-CMFB67U\GA2A <h1202415010@hiratagakuen.onmicrosoft.com>
2025-05-27 16:36:04 +0900

    会話実装途中

---
+++ teamproject/Assets/Script/Aiko_Script/NPC.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    private DialogText2 dialogue_text;
+
+    public GameObject talk_icon;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ teamproject/Assets/Script/Aiko_Script/Talk_Checker.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Talk_Checker : MonoBehaviour
+{
+    public NPC talk_npc;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            NPC target = collision.gameObject.GetComponent<NPC>();
+
+            talk_npc = target;
+
+            target.talk_icon.SetActive(true);
+
+            //DialogueManager.instance.isTalkable = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            NPC target = collision.gameObject.GetComponent<NPC>();
+
+         
+
+            target.talk_icon.SetActive(false);
+
+            if (talk_npc==target)
+            {
+                talk_npc = null;
+            }
+
+            //DialogueManager.instance.isTalkable = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
