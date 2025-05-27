1959b221fe74de8647464cfd36bd0a78bcf91666
DESKTOP-3GUQ93R\GA2A <h202419010@hiratagakuen.onmicrosoft.com>
2025-05-27 16:35:33 +0900

    UIを作り始めた

---
+++ teamproject/Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject mainImage;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
