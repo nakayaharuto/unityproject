e9d3979d5e1a508bac96b8126a0c9cd455db2bee
DESKTOP-U0TU0SG\GA2A <a>
2025-06-23 16:31:57 +0900

    パスワードオブジェクト完成

--- teamproject/Assets/machitani/script/PasswordScript.cs
+++ teamproject/Assets/machitani/script/PasswordScript.cs
@@ -6,44 +6,32 @@ public class PasswordScript : MonoBehaviour
 {
     private bool isNear;
     public GameObject text;
-    public GameObject Password1;
-    public GameObject Password2;
-    public GameObject Password3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        text.SetActive(false);
-        isNear=false;
+        isNear = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("f")&&Password1.CompareTag("box"))
-        {
-            Debug.Log("aaa");
-            text.SetActive(true);
-        }
-        if (Input.GetKeyDown("f") && Password2.CompareTag("Player"))
-        {
-            text.SetActive(true);
-        }
-        if (Input.GetKeyDown("f") && Password3.CompareTag("Player"))
+        if(Input.GetKeyDown("f")&&isNear)
         {
             text.SetActive(true);
         }
     }
-    private void OnTriggerEnter(Collider col)
+    void OnTriggerEnter(Collider col)
     {
-        if(col.tag=="Player")
+        if (col.tag == "Player")
         {
-            isNear=true; 
+            isNear = true;
         }
     }
-    void OnTriggerExit(Collider col)
+    void OnTriggerExit(Collider other)
     {
-        isNear =false;
+        isNear=false;
         text.SetActive(false);
     }
+
 }
