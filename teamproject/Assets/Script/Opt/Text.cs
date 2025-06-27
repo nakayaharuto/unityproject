af1a025bb7790309c1b57194a7718e363433a44e
DESKTOP-6HRT1DO\GA2A <h1202419010@hiratagauen.onmicrosoft.com>
2025-06-27 16:32:25 +0900

    シーンの変更

---
+++ teamproject/Assets/Script/Opt/Text.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    public Text text;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
